8f4d70d8042a03e4e208def034d9db9aed491a74
Alvaro <alvaro.saburido@gmail.com>
2022-07-21 10:27:49 +0200

    feat(ui): assets and sprite component

---
+++ packages/ui/src/components/pkmn-sprite/PkmnSprite.story.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+import PkmnSprite from './PkmnSprite.vue'
+
+const state = reactive({
+  nationalDex: 1,
+  shiny: false,
+  form: null,
+  egg: false,
+  size: 100,
+  icon: false,
+})
+</script>
+<template>
+  <Story
+    title="PkmnSprite"
+    :layout="{
+      type: 'grid',
+      width: 200,
+    }"
+  >
+    <Variant title="playground">
+      <PkmnSprite v-bind="state" />
+    </Variant>
+  </Story>
+</template>
---
+++ packages/ui/src/components/pkmn-sprite/PkmnSprite.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  nationalDex: {
+    default: 0,
+    type: Number,
+  },
+  size: {
+    default: 100,
+    type: Number,
+  },
+  shiny: {
+    default: false,
+    type: Boolean,
+  },
+  form: {
+    default: null,
+    type: String,
+  },
+  egg: {
+    default: false,
+    type: Boolean,
+  },
+  icon: {
+    default: false,
+    type: Boolean,
+  },
+})
+
+const paddedDex = computed(() =>
+  props.nationalDex < 10
+    ? `00${props.nationalDex}`
+    : props.nationalDex < 100
+    ? `0${props.nationalDex}`
+    : props.nationalDex,
+)
+
+const sprite = computed(() => {
+  let spriteIcon = paddedDex.value
+  if (props.shiny) {
+    spriteIcon += 's'
+  }
+  if (props.form) {
+    spriteIcon += `_${props.form}`
+  }
+  if (props.egg) {
+    spriteIcon = 'egg'
+  }
+  return `/pokemons/sprites/${spriteIcon}.png`
+})
+
+const styles = computed(() => ({
+  width: `${props.size}px`,
+  height: `${props.size}px`,
+}))
+</script>
+<template>
+  <img :src="sprite" :styles="styles" />
+</template>
